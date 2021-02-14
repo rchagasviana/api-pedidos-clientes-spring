f9578589fca602a05b8636d7c29cd1b838327af4
Rafael Viana <rchagas.sdi@gmail.com>
2021-02-14 09:16:07 -0300

    arquivos-base

---
+++ src/main/java/com/treinamentoSpring/api/ApiProdutosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.treinamentoSpring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProdutosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProdutosSpringApplication.class, args);
+	}
+
+}
